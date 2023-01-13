bfde869662aa309ef60bcf8213c1066b53ec33c6
UttoranRC <77979716+UttoranRC@users.noreply.github.com>
2023-01-13 21:05:08 +0530

    style update

--- src/component/profilePage.tsx
+++ src/component/profilePage.tsx
@@ -11,7 +11,9 @@ const ProfilePage: React.FC = () => {
             {
                 profData ?
                     profData?.filter(r => r.empId === id).map(r => (
-                        <div style={{ margin: '20px 150px', border: '1px solid black', borderRadius: '8px', textAlign: 'left', padding: '10px' }}>
+                        <div 
+                        style={{ margin: '20px 150px', border: '1px solid black', borderRadius: '8px', textAlign: 'left', fontWeight: 'bold', padding: '10px', background: 'linear-gradient(90deg, rgba(255,198,175,1) 1%, rgba(255,246,165,0.9585084033613446) 53%, rgba(206,255,130,1) 100%)' }}
+                        >
                             <div>Full Name: {r.fName + ' ' + r.lName}</div>
                             <div>Employee Id: {r.empId}</div>
                             <div>Date of Birth: {r.dob}</div>
